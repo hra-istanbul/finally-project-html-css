99ef10b6733522e280424cb2cdc17cc1a60bfe2d
Fatimei-Zehra <eliyevaf33@gmail.com>
2024-01-24 15:31:28 +0400

    Finished HomePage Final-Project-HTML-CSS

---
+++ js/index.js
@@ -0,0 +1,8 @@
+// var hours = document.getElementById("hours");
+// var min = document.getElementById("minutes");
+// var second = document.getElementById("seconds");
+
+// function timer(){
+
+// }
+
