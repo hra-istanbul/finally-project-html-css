028a2fa7ca0f01bfa7a44f8a816c0fa911ea37f4
Fatimei-Zehra <eliyevaf33@gmail.com>
2024-01-26 21:34:28 +0400

    FINISHED TABLET RESPONSIVE HOMEPAGE AND CREATE ACCOUNT

--- js/index.js
+++
@@ -1,8 +0,0 @@
-// var hours = document.getElementById("hours");
-// var min = document.getElementById("minutes");
-// var second = document.getElementById("seconds");
-
-// function timer(){
-
-// }
-
